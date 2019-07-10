137a415e1a04eb262080ce9124b5d9ff3864b8f4
Krops <andrekropes@yandex.ru>
2019-07-10 08:20:31 +0300

    Fix validator

--- src/views/pages/AddPost.js
+++ src/views/pages/AddPost.js
@@ -1,27 +1,27 @@
 var _ = require('lodash');
 let addPost = async (theme, description) => {
-  const options = {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: {
-        'theme': theme,
-        'description': description
-      }
-  };
-  try {
-      const response = await fetch(`http://localhost:4000/api/addPost`, options)
-       
-      const json = await response.json();
-      // console.log(json)
-      return json
-  } catch (err) {
-      console.log('Error getting documents', err)
-  }
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: {
+            'theme': theme,
+            'description': description
+        }
+    };
+    try {
+        const response = await fetch(`http://localhost:4000/api/addPost`, options)
+
+        const json = await response.json();
+        // console.log(json)
+        return json
+    } catch (err) {
+        console.log('Error getting documents', err)
+    }
 }
 let AddPost = {
-    render : async () => {
+    render: async () => {
         return /*html*/`
         <div id="postbody">
         
@@ -34,33 +34,44 @@ let AddPost = {
         `
     }
     , after_render: async () => {
-      document.getElementById("yes").addEventListener ("click",  () => {
-        let themeWarn = document.getElementById("themewarnId");
-        let descWarn = document.getElementById("descwarnId");
-        let theme       = document.getElementById("themeId");
-        let description        = document.getElementById("descrId");
-        if (theme.length < 5) {
-          themeWarn.innerHTML = "Theme should be greater then 5!";
-        }
-        else if (theme.length > 1000) {
-          themeWarn.innerHTML = "Theme should be smaller then 1000!";
-        }
-        if (description.length < 5) {
-          descWarn.innerHTML = "Description should be greater then 5!";
-        }
-        else if (description.length > 1000) {
-          descWarn.innerHTML = "Description should be smaller then 1000!";
-        } 
-        
-    })
-    // let response = await addPost(theme, description)
-    // if (_.isEmpty()){
-    //   document.getElementById("postbody").innerHTML = "Post added!"; 
-    // } else
-    // {
-    //   document.getElementById("warnId").innerHTML = response;
-    // }
-  }
+        document.getElementById("yes").addEventListener("click", () => {
+            let themeWarn = document.getElementById("themewarnId");
+            let descWarn = document.getElementById("descwarnId");
+            let theme = document.getElementById("themeId");
+            let description = document.getElementById("descrId");
+            if (theme.value.length < 5) {
+                themeWarn.innerText = "Theme should be greater then 5!";
+                console.log("1")
+            }
+            else if (theme.value.length > 1000) {
+                themeWarn.innerText = "Theme should be smaller then 1000!";
+                console.log("2")
+            }
+            if (description.value.length < 5) {
+                descWarn.innerText = "Description should be greater then 5!";
+                console.log("3")
+            }
+            else if (description.value.length > 1000) {
+                descWarn.innerText = "Description should be smaller then 1000!";
+                console.log("4")
+            } else if (5 < theme.value.length < 1000) {
+                themeWarn.innerText = "";
+                console.log("5")
+            }
+            else if (5 < description.value.length < 1000) {
+                descWarn.innerText = "";
+                console.log("6")
+            }
+
+        })
+        // let response = await addPost(theme, description)
+        // if (_.isEmpty()){
+        //   document.getElementById("postbody").innerHTML = "Post added!"; 
+        // } else
+        // {
+        //   document.getElementById("warnId").innerHTML = response;
+        // }
+    }
 }
 
 export default AddPost;
